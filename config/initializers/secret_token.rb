73fbbcf1d99fd04f5fa32b3c07c489138eff18aa
alex <alex@alex-HP-ProBook-4540s.(none)>
2013-02-17 14:48:21 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicSite::Application.config.secret_token = '563b7facf63b546adf70fc32be4c98cb1fbea7b88eb22a7da89930682f694538e9ba7bc03bbc5f878cd34395482d148c0d604e7d5be46bd69ad180beeefb9862'
